80d05e77cc79fe0ca158bd070d67487160200dc5
devyan18 <epache17@gmail.com>
2025-03-19 18:09:00 -0300

    FEAT - auth guard middleware

---
+++ src/middlewares/auth.guard.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, Response } from "express";
+import { AccessToken } from "../shared/tools/jwt";
+
+export async function authGuard(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.headers["AccessToken"] || req.cookies["AccessToken"];
+
+  if (!token) {
+    res.status(401).json({
+      message: "Invalid Credentials",
+    });
+    return;
+  }
+
+  const isValidToken = await AccessToken.validate(token);
+
+  if (!isValidToken) {
+    res.status(401).json({
+      message: "Invalid Credentials",
+    });
+    return;
+  }
+
+  next();
+}
